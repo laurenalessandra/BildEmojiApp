3e7030cf1b282e53d38b3a54c3a175c4bdcfc12f
Lauren Simon <laurenalessandra@hotmail.com>
2019-04-03 13:45:55 -0400

    Initial Commit

---
+++ BildEmojiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BildEmojiApp
+//
+//  Created by Lauren Simon on 4/3/19.
+//  Copyright © 2019 Lauren Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
