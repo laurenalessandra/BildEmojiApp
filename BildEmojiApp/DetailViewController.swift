9273ad8b7c2ccb82853c520ceacfee1ad1f24b8a
Lauren Simon <laurenalessandra@hotmail.com>
2019-04-06 12:45:06 -0400

    DetailViewController refactoring

--- BildEmojiApp/DetailViewController.swift
+++ BildEmojiApp/DetailViewController.swift
@@ -11,13 +11,17 @@ import UIKit
 class DetailViewController: UIViewController {
     
     var content: String!
+    var label = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        let label = UILabel()
-        label.text = content
         self.view.addSubview(label)
+        setUpView()
+    }
+    
+    func setUpView() {
+        view.backgroundColor = .white
+        label.text = content
         label.translatesAutoresizingMaskIntoConstraints = false
         label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
